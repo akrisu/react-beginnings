9019bef6ff4a40739c6d0bcf85db3e9d0c384269
krzysztofantecki <akrisu@gmail.com>
2018-05-18 11:39:25 +0200

    PR fixes

--- src/common/form/validators/validators.ts
+++ src/common/form/validators/validators.ts
@@ -1,5 +1,5 @@
 export type Validator = (value: string | undefined) => string | undefined
 
-export const required: Validator = (value: string | undefined) => (value ? undefined : 'Required')
-export const minLength: (min: number) => Validator =
-  (min: number) => (value: string) => ((value && value.length < min) ? `Must be at least ${min}` : undefined)
+export const required: Validator = value => (value ? undefined : 'Required')
+export const minLength: (min: number) => Validator = min => value =>
+  (value && value.length < min) ? `Must be at least ${min}` : undefined
--- src/lib/action-progress/pending-actions-manager.ts
+++ src/lib/action-progress/pending-actions-manager.ts
@@ -8,14 +8,12 @@ export interface IPendingActionsConfiguration {
 
 export const checkActionForPending = (configuration: IPendingActionsConfiguration[]) =>
   (state = Set<string>(), action: AnyAction): Set<string> => {
-    if (configuration.find(actionConfiguration => action.type === actionConfiguration.startOn)) {
+    if (configuration.find(({ startOn }) => action.type === startOn)) {
       return state.add(action.type)
     }
 
-    const actionToRemove = configuration.find(({stopOn}) =>
-      stopOn.some(actionType =>
-        actionType === action.type,
-      ))
+    const actionToRemove = configuration.find(({ stopOn }) =>
+      stopOn.some(actionType => actionType === action.type))
 
     if (actionToRemove) {
       return state.delete(actionToRemove.startOn)
